003520cd94f4b8cde53e0c43393e705dcfdae62c
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-03-28 10:30:51 -0400

    Collide angularly

--- src/Body.cpp
+++ src/Body.cpp
@@ -24,26 +24,6 @@ bool Body::collide(Body& other, scalar radius)
         scalar this_vel_y_adj = this->vel_x * sin + this->vel_y * cos;
         scalar other_vel_x_adj = other.vel_x * cos - other.vel_y * sin;
         scalar other_vel_y_adj = other.vel_x * sin + other.vel_y * cos;
-#if 0
-        scalar both_vel_y_avg = (this_vel_y_adj + other_vel_y_adj) / 2;
-        scalar both_vel_ang = (this_vel_y_adj - other_vel_y_adj) / radius / 2;
-        scalar both_vel_ang_new
-            = (3 * this->vel_ang + 3 * other.vel_ang + 18 * both_vel_ang
-                  - std::sqrt(3)
-                      * std::sqrt(7 * this->vel_ang * this->vel_ang
-                          - 2 * this->vel_ang * other.vel_ang
-                          + 7 * other.vel_ang * other.vel_ang
-                          - 12 * this->vel_ang * both_vel_ang
-                          - 12 * other.vel_ang * both_vel_ang
-                          + 12 * both_vel_ang * both_vel_ang))
-            / 24;
-        scalar one_vel_ang_new = (this->vel_ang + other.vel_ang) / 2
-            + 3 * both_vel_ang - 3 * both_vel_ang_new;
-        this_vel_y_adj = both_vel_y_avg + both_vel_ang_new * radius;
-        other_vel_y_adj = both_vel_y_avg - both_vel_ang_new * radius;
-        this->vel_ang = one_vel_ang_new;
-        other.vel_ang = one_vel_ang_new;
-#endif
         this->vel_x = other_vel_x_adj * cos - this_vel_y_adj * -sin;
         this->vel_y = other_vel_x_adj * -sin + this_vel_y_adj * cos;
         other.vel_x = this_vel_x_adj * cos - other_vel_y_adj * -sin;
@@ -56,9 +36,8 @@ bool Body::collide(Body& other, scalar radius)
         this->y -= correction_y;
         other.x += correction_x;
         other.y += correction_y;
-#if 1
+        // Collide angularly.
         std::swap(this->vel_ang, other.vel_ang);
-#endif
         return true;
     }
     return false;
