f23cb69bca693ec017b47562c45342979482acab
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-04-02 13:08:18 -0400

    Reduce middle layer
    I don't know how this will affect things.

--- src/Agent.hpp
+++ src/Agent.hpp
@@ -9,7 +9,7 @@ namespace bouncers {
 
 struct Agent {
     static constexpr int BRAIN_IN = 2 + 2 + 2 + 2 + 1;
-    static constexpr int BRAIN_MID = BRAIN_IN + 10;
+    static constexpr int BRAIN_MID = BRAIN_IN + 7;
     static constexpr int BRAIN_OUT = 2;
 
     Network<BRAIN_IN, BRAIN_MID, BRAIN_OUT> brain;
