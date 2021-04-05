500a2b8e440bf282c03465b6e883cb5cd92bdffa
Jude Melton-Houghton <jwmhjwmh@gmail.com>
2021-04-04 20:23:52 -0400

    Use SDL_Log instead of fprintf

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "conf.hpp"
 #include "simulate.hpp"
 #include <SDL2/SDL.h>
-#include <cstdio>
 
 using namespace bouncers;
 
@@ -11,21 +10,22 @@ int main(int argc, char* argv[])
     SDL_Window* window = nullptr;
     SDL_Renderer* renderer = nullptr;
     if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_EVENTS | SDL_INIT_VIDEO)) {
-        std::fprintf(stderr, "SDL initialization failed; %s\n", SDL_GetError());
+        SDL_LogCritical(SDL_LOG_CATEGORY_ERROR,
+            "SDL initialization failed; %s\n", SDL_GetError());
         goto error_sdl_init;
     }
     window = SDL_CreateWindow("Bouncers", SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED, conf::WINDOW_WIDTH, conf::WINDOW_HEIGHT,
         SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
     if (!window) {
-        std::fprintf(
-            stderr, "SDL window creation failed; %s\n", SDL_GetError());
+        SDL_LogCritical(SDL_LOG_CATEGORY_ERROR,
+            "SDL window creation failed; %s\n", SDL_GetError());
         goto error_create_window;
     }
     renderer = SDL_CreateRenderer(window, -1, 0);
     if (!renderer) {
-        std::fprintf(
-            stderr, "SDL renderer creation failed; %s\n", SDL_GetError());
+        SDL_LogCritical(SDL_LOG_CATEGORY_ERROR,
+            "SDL renderer creation failed; %s\n", SDL_GetError());
         goto error_create_surface;
     }
     simulate(renderer, argc >= 2 ? (unsigned)atoi(argv[1]) : 1337U);
